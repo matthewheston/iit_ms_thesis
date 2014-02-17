4a5855eae84bb20febcc64e2eee5adc41544c4a1
matthewheston <matthewheston@gmail.com>
2014-02-17 14:53:04 -0600

    Update feature set DDL to add new colummns contributor_count and repo_commits.

--- code/create_feature_set_table.sql
+++ code/create_feature_set_table.sql
@@ -1,12 +1,15 @@
 CREATE TABLE `feature_set` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `merged` int(1) DEFAULT NULL,
-  `user_activity` int(11) DEFAULT NULL,
+  `number_comments` int(11) DEFAULT NULL,
   `comments_on_pr` int(11) DEFAULT NULL,
-  `reputation` int(11) DEFAULT NULL,
+  `popularity` int(11) DEFAULT NULL,
+  `lsm` float DEFAULT NULL,
   `pr_id` int(11) unsigned NOT NULL,
   `commits` int(11) DEFAULT NULL,
   `changes` int(11) DEFAULT NULL,
+  `repo_commits` int(11) DEFAULT NULL,
+  `contributor_count` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `pr_fk` (`pr_id`),
   CONSTRAINT `pr_fk` FOREIGN KEY (`pr_id`) REFERENCES `pull_requests` (`id`)
